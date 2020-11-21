1977c2cbf31ab29402a6cdc0ed7f811a50aabf0d
alizavr <alina.dmitrievna2407@gmail.com>
2020-11-22 00:21:46 +0200

    added "clients"

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+       nav: true,
+       dots: false,
+  
+        responsive:{
+            0:{
+                items:1
+            },
+  
+            1000:{
+                items:1
+            }
+        }
+    });
+  });
+ 
