9283059a8af321aa89e1d40ec008333adb0885b4
alizavr <alina.dmitrievna2407@gmail.com>
2020-12-25 17:01:12 +0200

    added fancybox

--- src/js/app.js
+++ src/js/app.js
@@ -17,6 +17,19 @@ if(isMobile.any()){
 $( ".sub-region__item" ).on( "click", function() {
     $(this).toggleClass('actived');
 });
+$('[data-fancybox="gallery"]').fancybox({
+    buttons : [ 
+      'slideShow',
+      'share',
+      'zoom',
+      'fullScreen',
+      'download',
+      'close'
+    ],
+    
+    infobar: false,
+    
+  });
 
 $(document).ready(function(){
 
@@ -75,54 +88,3 @@ $('.header__burger').click(function(event){
         }
     }
 });
-// const popupLinks = document.querySelectorAll('.popup-link');
-// const body = document.querySelector('body');
-// const lockPadding = document.querySelectorAll('.lock-padding');
-// let unlock = true;
-// const timeout = 800;
-// if (popupLinks.length > 0){
-//     for(let index = 0; index < popupLinks.length; index++){
-//         const popupLinks = popupLinks[index];
-//         popupLinks.addEventLisstener('click', function(e){
-//             const popupName = popupLinks.getAttribute('href').replace('#','');
-//             const curentPopup = document.getElementById(popupName);
-//             popupOpen(curentPopup);
-//             e.preventDefault();
-//         }
-//         );
-//     }
-// }
-// const popupCloseIcon = document.querySelectorAll('.close-popup');
-// if (popupCloseIcon.length > 0){
-//     for (let index = 0; index < popupCloseIcon.length; index++){
-//         const el = popupCloseIcon[index];
-//         el.addEventListener('click', function(e){
-//             popupClose(el.closest('.popup'));
-//             e.preventDefault();
-//         });
-//     }
-// }
-// function popupOpen(curentPopup){
-//     if (curentPopup && unlock){
-//         const popupActive = document.querySelector('.popup.open');
-//         if(popupActive){
-//             popupClose(popupActive, false);
-//         } else {
-//             bodyLock();
-//         }
-//         curentPopup.classList.add('open');
-//         curentPopup.addEventListener('click', function(e){
-//             if (!e.target.closest('popup__content')){
-//                 popupClose(e.target.closest('.popup'));
-//             }
-//         });
-//     }
-// }
-// function popupClose(popupActive, doUnlock = true){
-//     if (unlock){
-//         popupActive.classList.remove('open');
-//         if (doUnlock){
-//             bodyUnLock
-//         }
-//     }
-// }
